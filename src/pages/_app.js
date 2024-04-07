197b3f9aa9e670a46b034ff0750a6ea3e81fad5e
Amir <amirhamza27940@gmail.com>
2024-04-07 15:28:11 +0600

    Added to github

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
+import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.css";
+import React from 'react'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const MyApp = ({Component, pageProps } ) => {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
+
+export default MyApp
