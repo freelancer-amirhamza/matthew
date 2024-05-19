edee01a377e1fce8dd5d9d6ac23b7d0c766f68b3
Amir <amirhamza27940@gmail.com>
2024-05-19 09:22:27 +0600

    ddd

---
+++ src/components/live/Live.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Live = () => {
+  return (
+    <>
+    <div className="w-full h-full flex flex-col mx-auto items-center justify-center ">
+        <div className=" ">
+
+        </div>
+        <div className=" ">
+            <div className=" ">
+                <h3 className=" ">
+                see Matthew 
+                <span>Live</span>
+                </h3>
+            </div>
+            <div className=" ">
+                <div className=" ">
+                    <div className="w-full h-full flex flex-col ">
+                        <img src="https://cdn-jnadd.nitrocdn.com/oelCgpbWzhAPlAFkDVhzQDcTKKHAUyGY/assets/images/optimized/rev-f702702/matthewhussey.com/wp-content/uploads/2021/10/sec9-img01.jpg" alt=""
+                        className=' relative' />
+                        <div className="absolute ">
+                            <h4 className=" ">the matthew hussey retreat:</h4>
+                            <h2 className=" ">Live Event </h2>
+                            <span className=" ">6 medical days 1000s of transformations </span>
+                            <button 
+                                className='md:w-6/12 w-full py-4 md:text-2xl text-3xl bg-primary border border-primary
+                                hover:bg-white hover:text-primary uppercase font-semibold tracking-wide text-white '
+                                type='submit'>learn more </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Live
